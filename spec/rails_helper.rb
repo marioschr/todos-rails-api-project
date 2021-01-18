49abd59b3cc47ee8ff35053f866efd570ab79e01
Marios Chr <marios-christou@hotmail.com>
2021-01-18 21:36:51 +0200

    Modified rails_helper.rb, added Authorize Api Request & Messages singleton

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -50,7 +50,8 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-  config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
   # add `FactoryBot` methods
   config.include FactoryBot::Syntax::Methods
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
